cd84c4a219f0d661b5fd5b96e781c893d0ea2f91
sauhardM <mishra.sauhard@gmail.com>
2022-03-12 01:46:21 +0530

    First commit

---
+++ src/script.js
@@ -0,0 +1,53 @@
+var timeStart = new Date().getTime();
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function makeShapeAppear() {
+  var top = Math.random() * 400;
+  document.getElementById("shape").style.top = top + "px";
+
+  var left = Math.random() * 400;
+  document.getElementById("shape").style.left = left + "px";
+
+  var width = Math.random() * 200 + 100;
+  document.getElementById("shape").style.width = width + "px";
+
+  if (Math.random() > 0.5) {
+    document.getElementById("shape").style.borderRadius = "50%";
+  } else {
+    document.getElementById("shape").style.borderRadius = "0";
+  }
+
+  document.getElementById("shape").style.backgroundColor = getRandomColor();
+
+  document.getElementById("shape").style.height = width + "px";
+
+  document.getElementById("shape").style.display = "block";
+
+  timeStart = new Date().getTime();
+}
+
+function appearAfterDelay() {
+  setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+
+appearAfterDelay();
+
+document.getElementById("shape").onclick = function () {
+  document.getElementById("shape").style.display = "none";
+
+  var timeEnd = new Date().getTime();
+
+  var timeTaken = (timeEnd - timeStart) / 1000;
+
+  document.getElementById("timeTaken").innerHTML = timeTaken + " seconds";
+
+  appearAfterDelay();
+};
